20eb88d7c0ae24fb0eee80cc0deffea1b7619c2e
Ekaterina <katerina3@mail.ru>
2023-02-19 18:02:26 +0300

    task01classupdate

--- task01class/Program.cs
+++ task01class/Program.cs
@@ -5,30 +5,23 @@ int InputNumber(string str) //ввод числа string - тип перемен
 {
     System.Console.WriteLine(str);
     return Convert.ToInt32(Console.ReadLine());
-}  
+}
 
-int X = InputNumber("Введиде координату X: ");
-int Y = InputNumber("Введиде координату Y: ");
-
-// System.Console.Write("Введите координату X:  ");
-// int X = Convert.ToInt32(Console.ReadLine());
-
-// System.Console.Write("Введите координату Y:  ");
-// int Y = Convert.ToInt32(Console.ReadLine());
-
-if(X > 0 && Y > 0)
+void FindQuarter(int a, int b) //принимает две какие-то переменные
+{
+    if (a > 0 && b > 0)
 {
     System.Console.WriteLine("Номер четверти 1");
 }
-else if(X < 0 && Y > 0)
+else if (a < 0 && b > 0)
 {
     System.Console.WriteLine("Номер четверти 2");
 }
-else if(X < 0 && Y < 0)
+else if (a < 0 && b < 0)
 {
     System.Console.WriteLine("Номер четверти 3");
-}   
-else if(X > 0 && Y < 0)
+}
+else if (a > 0 && b < 0)
 {
     System.Console.WriteLine("Номер четверти 4");
 }
@@ -36,3 +29,38 @@ else
 {
     System.Console.WriteLine("Точка лежит в начале координат");
 }
+
+}
+
+int X = InputNumber("Введиде координату X: ");
+int Y = InputNumber("Введиде координату Y: ");
+
+FindQuarter(X, Y);
+FindQuarter(-4, 7);
+
+// // System.Console.Write("Введите координату X:  ");
+// // int X = Convert.ToInt32(Console.ReadLine());
+
+// // System.Console.Write("Введите координату Y:  ");
+// // int Y = Convert.ToInt32(Console.ReadLine());
+
+// if (X > 0 && Y > 0)
+// {
+//     System.Console.WriteLine("Номер четверти 1");
+// }
+// else if (X < 0 && Y > 0)
+// {
+//     System.Console.WriteLine("Номер четверти 2");
+// }
+// else if (X < 0 && Y < 0)
+// {
+//     System.Console.WriteLine("Номер четверти 3");
+// }
+// else if (X > 0 && Y < 0)
+// {
+//     System.Console.WriteLine("Номер четверти 4");
+// }
+// else
+// {
+//     System.Console.WriteLine("Точка лежит в начале координат");
+// }
